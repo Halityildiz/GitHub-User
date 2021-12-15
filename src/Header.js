57b0f48980ebe4b799a1ae8672782f2b1d6c801e
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-12-15 21:55:55 +0300

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import logo from "./logo.svg";
+const Header = () => {
+  return (
+    <div className="section-title">
+      <img
+        src="https://i.pinimg.com/564x/e3/f9/c4/e3f9c42f3b449e72123466082ae3eb18.jpg"
+        className="App-logo"
+        alt="logo"
+      />
+
+      <h1>Github Users</h1>
+    </div>
+  );
+};
+
+export default Header;
